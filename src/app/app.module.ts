1ac0591840c3bf147d514ad9d0bebe29c5ea1190
Nicholas-muchiri <nickromero187@gmail.com>
2018-06-17 11:21:49 +0300

    App module.ts updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RoutingModule } from './routing/routing.module'
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { ReposComponent } from './repos/repos.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DataService } from './services/data.service';
+import { HttpModule } from '@angular/http';
+import { FontDirective } from './font-directive/font.directive';
+import { LastPipe } from './pipe/last.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        UsersComponent,
+        ReposComponent,
+        NotFoundComponent,
+        FontDirective,
+        LastPipe
+    ],
+    imports: [
+        BrowserModule,
+        RoutingModule,
+        HttpModule,
+        FormsModule
+
+    ],
+    providers: [DataService],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
